98d1e67256cfedf39a511c08622bbcfe12a2f8f0
Ignacio Valencia <86011934+ignacio-valencia@users.noreply.github.com>
2023-09-28 22:47:42 -0300

    fix

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
 const core = require('@actions/core');
 
-async function run() {
-    const file1Path = process.env.INPUT_FILE1;
-    const file2Path = process.env.INPUT_FILE2;
+function isFilePath(input) {
+    return input.endsWith('.json');
+}
 
-    const file1Content = JSON.parse(fs.readFileSync(file1Path, 'utf8'));
-    const file2Content = JSON.parse(fs.readFileSync(file2Path, 'utf8'));
+function getJsonContent(input) {
+    if (isFilePath(input)) {
+        return JSON.parse(fs.readFileSync(input, 'utf8'));
+    } else {
+        return JSON.parse(input);
+    }
+}
+
+async function run() {
+    const file1Input = process.env.INPUT_FILE1;
+    const file2Input = process.env.INPUT_FILE2;
+
+    const file1Content = getJsonContent(file1Input);
+    const file2Content = getJsonContent(file2Input);
 
     const mergedContent = {};
     Object.keys(file2Content).forEach(key => {
